42d3de7dbe837cd40ef40f6f5d46bf7ce4fabde6
itamar Arjuan <itamar.arjuan@gmail.com>
2019-09-02 14:49:50 +0300

    adding code for v2 in a separate v2 branch

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/itamararjuan/go-modules-project-b
+module github.com/itamararjuan/go-modules-project-b/v2
 
 go 1.12
--- say_hello.go
+++ say_hello.go
@@ -1,7 +1,14 @@
 package b
 
+import "fmt"
+
 // SayHelloInEnglish - Hello saying function
 // Use this method to return a text saying Hello in English
-func SayHelloInEnglish() string {
-	return "Saying hello.."
+func SayHelloInEnglish(name string) string {
+	return fmt.Sprintf("Hello %s", name)
+}
+
+// SayHelloInSpanish - Says hello in Spanish
+func SayHelloInSpanish() string {
+	return "Hola"
 }
